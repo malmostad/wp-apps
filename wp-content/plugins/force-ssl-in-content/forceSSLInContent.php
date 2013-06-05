6ac611e43a155f588e42f3eb107a33a8242d93fd
martent <marten@thavenius.se>
2013-06-05 11:17:21 +0200

    Plug for replacing http: to https: in content

---
+++ wp-content/plugins/force-ssl-in-content/forceSSLInContent.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  Plugin Name: Force SSL in Content
+  Plugin URI: https://github.com/malmostad/intranet-wordpress-themes
+  Description: Replace http: with https: for inline images and media content from the same WP app
+               Note: Images and media from other servers is not changed since we do not know if
+               other servers support https:
+  Version: 0.0.1
+  Author: Malmö stad
+  Author URI: https://github.com/malmostad
+*/
+
+add_filter('the_content', 'forceSSL');
+function forceSSL($content) {
+  $content = ereg_replace("http://" . $_SERVER["SERVER_NAME"], "https://" . $_SERVER["SERVER_NAME"], $content);
+  return $content;
+}
