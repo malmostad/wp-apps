b0c340d35ce5ec23076e7f91bf6bdd456ec17691
martent <marten@thavenius.se>
2013-12-04 16:15:53 +0100

    Cleanup of intranet specifics

--- wp-content/themes/external-blogg/sidebar-footer.php
+++ wp-content/themes/external-blogg/sidebar-footer.php
@@ -1,3 +1,21 @@
-<?php global $mconfig; ?>
 <!--eri-no-index-->
+<section class="categories" role="main">
+  <?php
+    // Split categories in 4 columns
+    $items = get_terms('category', array( 'parent' => 0, 'hide_empty' => false));
+    $cols = array_chunk($items, ceil(count($items) / 4));
+
+    for ($i=0; $i < count($cols); $i++) {
+      echo "<ul class='col-{$i}'>";
+      foreach ($cols[$i] as $item) {
+        printf('<li><a href="%2$s">%1$s</a> <span class="count">(%3$d)</span></li>',
+          $item->name,
+          get_term_link($item, 'category'),
+          $item->count
+        );
+      }
+      echo '</ul>';
+    }
+  ?>
+</section>
 <!--/eri-no-index-->
--- wp-content/themes/external-blogg/single.php
+++ wp-content/themes/external-blogg/single.php
@@ -42,9 +42,6 @@
           categories: [ <?php foreach((get_the_category()) as $category) { echo '"' . htmlspecialchars_decode($category->cat_name) . '",'; } ?> ]
         };
       </script>
-      <div <?php if (GetVotes(get_the_ID())) { echo 'data-vote="' . get_the_ID() . '" class="vote-box"'; } ?>>
-        <?php DisplayVotes(get_the_ID()); ?>
-      </div>
     </section>
 
     <article class="body-copy">
