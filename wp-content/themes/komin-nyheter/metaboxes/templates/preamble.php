d4fa09652ef48a75d960f6c1d35ff53635be9af2
martent <marten@thavenius.se>
2014-01-13 12:40:44 +0100

    Limit width for preamble. #9

--- wp-content/themes/komin-nyheter/metaboxes/templates/preamble.php
+++ wp-content/themes/komin-nyheter/metaboxes/templates/preamble.php
@@ -3,7 +3,7 @@
 <div class="mb_control">
 	<label for="<?php $metabox->the_name('preamble'); ?>">Ingress <span>(obligatoriskt)</span></label>
 	<p>
-		<textarea id="<?php $metabox->the_name('preamble'); ?>" name="<?php $metabox->the_name('preamble'); ?>"  style="width: <?php echo $content_width ?>px" rows="6"><?php $metabox->the_value('preamble') ?></textarea>
+		<textarea id="<?php $metabox->the_name('preamble'); ?>" name="<?php $metabox->the_name('preamble'); ?>" rows="6"><?php $metabox->the_value('preamble') ?></textarea>
     <span>[Eventuella ytterligare instruktioner]</span>
 	</p>
 </div>
