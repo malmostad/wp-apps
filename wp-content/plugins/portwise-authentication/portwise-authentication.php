ac3b204d0e00cfbf1c6c1d2c94d7cce165dcaed3
martent <marten@thavenius.se>
2014-04-07 15:22:15 +0200

    Create and update authenticated user

--- wp-content/plugins/portwise-authentication/portwise-authentication.php
+++ wp-content/plugins/portwise-authentication/portwise-authentication.php
@@ -3,6 +3,8 @@
   Plugin Name: Portwise Authentication
   Plugin URI: https://github.com/malmostad/intranet-wordpress-themes
   Description: Authentication with Portwise reveresed proxy http headers method
+               If user is present, it is updated with attributes from Portwise
+               If user isn't present, it is created with attributes from Portwise
   Version: 0.0.1
   Author: Malmö stad
   Author URI: https://github.com/malmostad
@@ -20,20 +22,26 @@ class PortwiseAuthentication {
   function __construct() {
     add_action('after_setup_theme', array($this, 'signon'));
     add_action('wp_logout', array($this, 'signout'));
-    error_log("TRUST: " . $this->trust_request());
+    $_SERVER['HTTP_X_TOKEN'] = PORTWISE_TOKEN;
+    $_SERVER['HTTP_X_UID'] = "martha2";
   }
 
   public function signon() {
     error_log("portwise_authentication");
-    if (!is_user_logged_in()) {
+    if ($this->trust_request() && !is_user_logged_in()) {
       error_log("START AUTH");
-      $username = 'admin';
-      if (get_user_by('login', $username)) {
-        $user = get_user_by('login', $username);
+
+      if (username_exists($this->username)) {
+        $user = get_user_by('login', $this->$username);
+        $this->update_user($user);
+      } else {
+        $user = $this->add_user();
+      }
+
+      if ($user) {
         wp_set_current_user($user->ID, $user->user_login);
         wp_set_auth_cookie($user->ID, false, PORTWISE_REQUIRE_SSL);
         do_action('wp_login', $user->user_login);
-        error_log("is_user_logged_in: " . is_user_logged_in());
       }
     }
   }
@@ -47,15 +55,43 @@ class PortwiseAuthentication {
   private function trust_request() {
     return ($_SERVER['REMOTE_ADDR'] == PORTWISE_IP_ADDRESS && // HTTP_CLIENT_IP HTTP_X_FORWARDED_FOR
         $_SERVER['HTTP_X_TOKEN'] == PORTWISE_TOKEN &&
-        !empty($_SERVER['HTTP_X_UID']) &&
+        isset($_SERVER['HTTP_X_UID']) &&
         (PORTWISE_REQUIRE_SSL ? $_SERVER['HTTPS'] == "on" : true));
   }
 
-  private function add_user($userdata) {
-    wp_insert_user($userdata);
+  private function add_user() {
+    $id = wp_insert_user(array(
+      'user_login'   => $this->username(),
+      'user_email'   => $this->email(),
+      'nickname'     => $this->nickname(),
+      'display_name' => $this->display_name(),
+      'user_pass'    => wp_generate_password(64, true, true) // unusable pw
+    ));
+    return get_user_by('id', $id);
   }
 
-  private function update_user($userdata) {
-    wp_update_user($userdata);
+  private function update_user($user) {
+    return wp_update_user(array(
+      'id'           => $user->ID,
+      'user_email'   => $this->email(),
+      'nickname'     => $this->nickname(),
+      'display_name' => $this->display_name()
+    ));
+  }
+
+  private function username() {
+    return $_SERVER['HTTP_X_UID'];
+  }
+
+  private function display_name() {
+    return $_SERVER['HTTP_X_DISPLAYNAME']? $_SERVER['HTTP_X_DISPLAYNAME'] : $_SERVER['HTTP_X_UID'];
+  }
+
+  private function nickname() {
+    return $this->displayname();
+  }
+
+  private function email() {
+    return $_SERVER['HTTP_X_EMAIL']? $_SERVER['HTTP_X_EMAIL'] : "{$_SERVER['HTTP_X_UID']}@malmo.se";
   }
 }
