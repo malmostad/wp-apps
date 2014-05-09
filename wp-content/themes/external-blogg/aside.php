31e892f228fa74599211fd34318a5601dfc9caf2
martent <marten@thavenius.se>
2014-05-09 17:06:22 +0200

    Aside cleanup

--- wp-content/themes/external-blogg/aside.php
+++ wp-content/themes/external-blogg/aside.php
@@ -1,5 +1,5 @@
 <!--eri-no-index-->
-<aside class="basic" role="complementary">
+<aside role="complementary">
   <?php if (is_home() && is_active_sidebar('home-page-widget-area')): ?>
     <div class="widget-area">
       <?php dynamic_sidebar('home-page-widget-area') ?>
@@ -7,28 +7,31 @@
   <?php endif ?>
 
   <?php if (!is_home()): ?>
-    <h1><a href="<?php bloginfo("url")?>">Senaste inläggen</a></h1>
-    <ul class="latest-post">
-      <?php
-        global $template_vars;
-        $query = get_top_posts($template_vars['postID']);
-        if ($query->have_posts()):
-          while ($query->have_posts()): $query->the_post(); ?>
-          <li>
-            <a href="<?php echo get_permalink() ?>">
-              <div class="featured-image">
-                <?php echo get_avatar(get_the_author_meta('user_email'), 48) ?>
-              </div>
-              <h2><?php the_title() ?></h2>
-              <time><?php echo get_the_date() . ' ' .  get_the_time() ?></time>
-            </a>
-          </li>
-        <?php endwhile ?>
-      <?php endif ?>
-    </ul>
+    <nav class="basic latest-posts">
+      <h1><a href="<?php bloginfo("url")?>">Senaste inläggen</a></h1>
+      <ul>
+        <?php
+          global $template_vars;
+          $query = get_top_posts($template_vars['postID']);
+          if ($query->have_posts()):
+            while ($query->have_posts()): $query->the_post(); ?>
+            <li>
+              <a href="<?php echo get_permalink() ?>">
+                <div class="featured-image">
+                  <?php echo get_avatar(get_the_author_meta('user_email'), 48) ?>
+                </div>
+                <p><?php the_title() ?>
+                  <time><?php echo get_the_date() . ' ' .  get_the_time() ?></time>
+                </p>
+              </a>
+            </li>
+          <?php endwhile ?>
+        <?php endif ?>
+      </ul>
+    </nav>
   <?php endif ?>
 
-  <nav class="basic">
+  <nav class="basic ">
     <h1>Här hittar du</h1>
     <ul>
       <li><a href="<?php bloginfo('url'); ?>/">Alla inlägg</a></li>
