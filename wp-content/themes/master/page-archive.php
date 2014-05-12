4b69267171de284e4baa792be8085e5c3aa7dbc8
martent <marten@thavenius.se>
2014-05-12 12:14:17 +0200

    Markup changes for archive pages in master theme

--- wp-content/themes/master/page-archive.php
+++ wp-content/themes/master/page-archive.php
@@ -6,9 +6,11 @@
 ?>
 <main class="archive" role="main">
   <h1 class="body-copy"><?php echo get_the_title(); ?></h1>
-  <ul>
-    <?php wp_get_archives( array('show_post_count' => true) ); ?>
-  </ul>
+  <nav class="basic">
+    <ul>
+      <?php wp_get_archives( array('show_post_count' => true) ); ?>
+    </ul>
+  </nav>
 </main>
 <?php get_template_part('aside'); ?>
 <?php get_footer(); ?>
--- wp-content/themes/master/page-categories.php
+++ wp-content/themes/master/page-categories.php
@@ -6,23 +6,25 @@
 ?>
 <main class="categories" role="main">
   <h1 class="body-copy"><?php echo get_the_title(); ?></h1>
-  <?php
-    // Split categories in 4 columns
-    $items = get_terms('category', array( 'parent' => 0, 'hide_empty' => false));
-    $cols = array_chunk($items, ceil(count($items) / 4));
+  <nav class="basic">
+    <?php
+      // Split categories in 4 columns
+      $items = get_terms('category', array( 'parent' => 0, 'hide_empty' => false));
+      $cols = array_chunk($items, ceil(count($items) / 4));
 
-    for ($i=0; $i < count($cols); $i++) {
-      echo "<ul class='col-{$i}'>";
-      foreach ($cols[$i] as $item) {
-        printf('<li><a href="%2$s">%1$s</a> <span class="count">(%3$d)</span></li>',
-          $item->name,
-          get_term_link($item, 'category'),
-          $item->count
-        );
+      for ($i=0; $i < count($cols); $i++) {
+        echo "<ul class='col-{$i}'>";
+        foreach ($cols[$i] as $item) {
+          printf('<li><a href="%2$s">%1$s</a> <span class="count">(%3$d)</span></li>',
+            $item->name,
+            get_term_link($item, 'category'),
+            $item->count
+          );
+        }
+        echo '</ul>';
       }
-      echo '</ul>';
-    }
-  ?>
+    ?>
+  </nav>
 </main>
 <?php get_template_part('aside'); ?>
 <?php get_footer(); ?>
