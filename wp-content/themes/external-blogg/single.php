3a10a3f59b20e56963d34f59a0f31567845ef738
martent <marten@thavenius.se>
2014-05-12 16:44:53 +0200

    Breakpoints for article images

--- wp-content/themes/external-blogg/single.php
+++ wp-content/themes/external-blogg/single.php
@@ -32,17 +32,14 @@
 
     <div class="article-and-comments">
       <article class="body-copy">
-        <?php if ( has_post_thumbnail() ) { ?>
+        <?php if (has_post_thumbnail()): ?>
           <div class="featured-image">
             <?php the_post_thumbnail('medium', array('class' => 'article-image')) ?>
           </div>
-          <?php }
-            the_content();
-            $postID = $post->ID;
-            if (is_user_logged_in()):
-          ?>
-        <?php endif; ?>
-
+        <?php endif;
+          the_content();
+          $postID = $post->ID;
+        ?>
         <?php get_template_part('single-meta') ?>
       </article>
       <?php comments_template( '', true ); ?>
