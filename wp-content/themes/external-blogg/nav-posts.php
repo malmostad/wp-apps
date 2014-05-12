0e9b9c36cec00c0721c99ebdb0043b53bfd7a265
martent <marten@thavenius.se>
2014-05-12 16:21:02 +0200

    Moved media queries into context

--- wp-content/themes/external-blogg/nav-posts.php
+++ wp-content/themes/external-blogg/nav-posts.php
@@ -3,10 +3,10 @@ global $wp_query;
 if ($wp_query->max_num_pages > 1): ?>
   <nav>
     <ul class="pagination">
-      <li class="previous"><?php next_posts_link('Äldre inlägg') ?></li>
+      <li class="previous" rel="prev"><?php next_posts_link('Äldre inlägg') ?></li>
       <?php if (strlen(get_previous_posts_link())): ?>
-        <li class="next"><?php previous_posts_link('Nyare inlägg') ?></li>
+        <li class="next" rel="next"><?php previous_posts_link('Nyare inlägg') ?></li>
       <?php endif ?>
     </ul>
   </nav>
-<?php endif; ?>
+<?php endif ?>
