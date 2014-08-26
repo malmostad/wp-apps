c83d18fbe964fd839976b5fefc08a2cbc0e0e86e
martent <marten@thavenius.se>
2014-08-26 17:20:43 +0200

    Removed placeholder text

--- wp-content/themes/komin-nyheter/metaboxes/templates/preamble.php
+++ wp-content/themes/komin-nyheter/metaboxes/templates/preamble.php
@@ -4,6 +4,6 @@
 	<label for="<?php $metabox->the_name('preamble'); ?>">Ingress <span>(obligatoriskt)</span></label>
 	<p>
 		<textarea id="<?php $metabox->the_name('preamble'); ?>" name="<?php $metabox->the_name('preamble'); ?>" rows="6"><?php $metabox->the_value('preamble') ?></textarea>
-    <span>[Eventuella ytterligare instruktioner]</span>
+    <span><!-- [Eventuella ytterligare instruktioner] --></span>
 	</p>
 </div>
