a24391aacbf97d7c396074b09740dae3b1aa56f0
martent <marten@thavenius.se>
2014-08-28 12:19:52 +0200

    Upgraded metaboxes config to TinyMCE API 4

--- wp-content/themes/komin-nyheter/metaboxes/metaboxes.js
+++ wp-content/themes/komin-nyheter/metaboxes/metaboxes.js
@@ -9,37 +9,17 @@ jQuery(function($) {
     }
     attempts++;
     // We need to loop since the #content editor has no initiated callback
-    if (tinyMCE.get('content') === undefined) {
+    if (tinyMCE.get('content') === null) {
       setTimeout(function() {
         setupEditor();
       }, 10);
     } else {
       var settings = tinyMCE.get('content').settings;
+
       $.extend(settings, {
-        mode: "exact",
-        elements: "metabox_facts",
-        theme_advanced_buttons1: "undo,redo,|,italic,|,bullist,numlist,outdent,indent,blockquote,|,pastetext,pasteword,removeformat",
-        theme_advanced_buttons3: "",
-        theme_advanced_buttons4: "",
-        // plugins: settings.plugins + ",table",
-        // theme_advanced_buttons2: "tablecontrols",
-        // table_styles: "",
-        // table_cell_styles: "",
-        // table_row_styles: "",
-        paste_remove_spans: true,
-        paste_remove_styles: true,
-        paste_strip_class_attributes: "all",
-        paste_text_use_dialog: true,
-        //valid_elements: 'em/i',
-        paste_text_sticky : true,
-        remove_linebreaks : false,
-        setup : function(editor) {
-          editor.onInit.add(function(editor) {
-            editor.pasteAsPlainText = true;
-          });
-        }
+        selector: "#metabox_facts"
       });
-      tinyMCE.init( settings );
+      tinyMCE.init(settings);
     }
   };
 
@@ -48,10 +28,9 @@ jQuery(function($) {
     setupEditor();
   }
 
-  $('#wpa_loop-links').delegate('.link-url input', 'focus', function() {
+  $('#wpa_loop-links').on('focus', '.link-url input', function() {
     if ($(this).val() === '') {
       $(this).attr('value', 'http://');
     }
   });
-
 });
