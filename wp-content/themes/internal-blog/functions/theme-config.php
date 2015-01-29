ecfa23dc8b56d444f49c71e4de5b0f0cfa37ac8c
martent <marten@thavenius.se>
2015-01-29 14:30:58 +0100

    Theme configs

--- wp-content/themes/internal-blog/functions/theme-config.php
+++ wp-content/themes/internal-blog/functions/theme-config.php
@@ -1,24 +1,16 @@
 <?php
 
-$mconfig['env'] = ENV;
-$mconfig['logdir'] = dirname(__FILE__) . '/../../../';
-
 $mconfig['staff_directory'] = 'http://webapps06.malmo.se/dashboard/users/';
 $mconfig['avtar_base_url'] =  '//webapps06.malmo.se/avatars/';
 
 if ($mconfig['env'] == 'development') {
   $mconfig['asset_host'] = '//assets.malmo.se/internal/v4-staging/';
-  $mconfig['loglevel'] = 3;
 }
 elseif ($mconfig['env'] == 'test' ) {
   $mconfig['asset_host'] = '//assets.malmo.se/internal/v4-staging/';
-  $mconfig['loglevel'] = 2;
-  $mconfig['cache_engine'] = 'apc';
 }
 else {
   $mconfig['asset_host'] = '//assets.malmo.se/internal/v4/';
-  $mconfig['loglevel'] = 1;
-  $mconfig['cache_engine'] = 'apc';
 }
 
 $mconfig['cache_id'] = 'internal_blog_' . $mconfig['env'];
