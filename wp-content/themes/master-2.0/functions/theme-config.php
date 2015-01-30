df2b27cc99f4d982481fcfe7115f316dab2f6c67
martent <marten@thavenius.se>
2015-01-30 12:40:24 +0100

    config

--- wp-content/themes/master-2.0/functions/theme-config.php
+++ wp-content/themes/master-2.0/functions/theme-config.php
@@ -6,9 +6,8 @@ $mconfig['logdir'] = dirname(__FILE__) . '/../../../';
 if (empty($mconfig['asset_host'])) { // To make it possible to override it in child theme
   if ( $mconfig['env'] == 'development') {
 
-    $mconfig['avtar_base_url'] =  '//www.local.malmo.se/ws/avatars/';
-    $mconfig['asset_host'] =  '//www.local.malmo.se:3001/assets/';
-    $mconfig['staff_directory'] = 'http://www.local.malmo.se:3000/users/';
+    $mconfig['avtar_base_url'] =  '//webapps06.malmo.se/avatars/';
+    $mconfig['asset_host'] = '//assets.malmo.se/internal/3.0-staging/';
     $mconfig['staff_directory'] = 'http://webapps06.malmo.se/dashboard/users/';
     $mconfig['loglevel'] = 3;
   }
