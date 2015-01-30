92a8ed6ee659a6a91339a65ddfe1f97f952bd45c
martent <marten@thavenius.se>
2015-01-30 15:21:44 +0100

    Debugged meta boxes

--- wp-content/themes/internal-news/metaboxes/templates/facts.php
+++ wp-content/themes/internal-news/metaboxes/templates/facts.php
@@ -1,12 +1,6 @@
-<?php
-  global $content_width;
-  $facts = get_post_meta($post->ID, '_metabox_facts', true);
-?>
-
+<?php global $content_width; ?>
 <div class="mb_control customEditor">
   <p>I faktarutan bryter du ut innehåll som innehåller bakgrundsinformation och liknande.</p>
   <label class="hide-if-js" for="<?php $metabox->the_name('facts'); ?>">Faktaruta<span> (frivilligt)</span></label>
-	<textarea id="metabox_facts" name="<?php $metabox->the_name('facts'); ?>"  style="width: <?php echo $content_width ?>px" rows="14"><?php echo wp_richedit_pre($facts['facts']); ?></textarea>
+	<textarea id="metabox_facts" name="<?php $metabox->the_name('facts'); ?>"  style="width: <?php echo $content_width ?>px" rows="14"><?php echo wp_richedit_pre($metabox->the_value('facts')) ?></textarea>
 </div>
-
-
--- wp-content/themes/internal-news/metaboxes/templates/preamble.php
+++ wp-content/themes/internal-news/metaboxes/templates/preamble.php
@@ -1,9 +1,7 @@
 <?php global $content_width; ?>
-
 <div class="mb_control">
 	<label for="<?php $metabox->the_name('preamble'); ?>">Ingress <span>(obligatoriskt)</span></label>
 	<p>
 		<textarea id="<?php $metabox->the_name('preamble'); ?>" name="<?php $metabox->the_name('preamble'); ?>" rows="6"><?php $metabox->the_value('preamble') ?></textarea>
-    <span>[Eventuella ytterligare instruktioner]</span>
 	</p>
 </div>
