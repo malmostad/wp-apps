78c018b2cf87373c54fe1f86fb9d5a2770c3e79e
martent <marten@thavenius.se>
2015-02-02 11:29:37 +0100

    Changed menu for internal blog

--- wp-content/themes/internal-blog/aside.php
+++ wp-content/themes/internal-blog/aside.php
@@ -27,7 +27,7 @@
   <?php endif ?>
 
   <nav class="basic ">
-    <h1>Blogga</h1>
+    <h1>Blogg</h1>
     <ul>
       <?php if (is_user_logged_in()): ?>
         <li><a href="<?php bloginfo("url")?>/wp-admin/post-new.php">Skapa ett blogginlägg</a></li>
@@ -38,7 +38,6 @@
       <li><a href="https://webapps09.malmo.se/wiki/Blogga_p%C3%A5_Komin">Hjälp</a></li>
     </ul>
 
-    <h1>Här hittar du</h1>
     <ul>
       <li><a href="<?php bloginfo('url'); ?>/">Alla inlägg</a></li>
       <li><a href="<?php bloginfo('url'); ?>/bloggare">Bloggare</a></li>
