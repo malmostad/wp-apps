bb90fe2319f6854212fc54c466f04c6a0affe175
martent <marten@thavenius.se>
2015-02-10 10:30:16 +0100

    Deploy config for external blog

--- config/deploy/external-blog-staging.rb
+++ config/deploy/external-blog-staging.rb
@@ -1,4 +1,4 @@
 set :server_address, 'webapps07.malmo.se'
 server server_address, :web
 set :theme, 'external-blog'
-set :deploy_to, "~/linked-wp-apps/#{theme}-theme/production"
+set :deploy_to, "/home/marten/#{theme}-theme/staging"
