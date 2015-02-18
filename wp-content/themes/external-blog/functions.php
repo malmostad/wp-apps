1298a67a8b16688efe9d4a287c7487558407d7a7
martent <marten@thavenius.se>
2015-02-18 20:22:55 +0100

    Moved gravatar image include for RSS to external-blog theme

--- wp-content/themes/external-blog/functions.php
+++ wp-content/themes/external-blog/functions.php
@@ -1,3 +1,4 @@
 <?php
 /* Functions for child theme */
 require_once 'functions/theme-config.php';
+require_once 'functions/hooks.php';
--- wp-content/themes/master/functions/hooks.php
+++ wp-content/themes/master/functions/hooks.php
@@ -148,24 +148,6 @@ function let_child_rule() {
   return true;
 }
 
-/* Add gravatar image to rss feed */
-function wp_rss_img_include($comment) {
-  $email = $comment ? get_comment_author_email() : get_the_author_meta('user_email');
-  $email = md5( strtolower( trim($email) ) );
-  if ( is_ssl() ) {
-    $avatar_url = 'https://secure.gravatar.com';
-  } else {
-    $avatar_url = 'http://1.gravatar.com';
-  }
-  $avatar_url .= '/avatar/' . $email . '?d=mm&amp;s=100.jpg';
-  echo '<enclosure url="' . $avatar_url . '" type="image/jpeg"/>';
-}
-function wp_rss_img_do_feed(){
-  add_action('rss2_item', 'wp_rss_img_include');
-  add_action('commentrss2_item', 'wp_rss_img_include');
-}
-add_action("do_feed_rss2","wp_rss_img_do_feed",5,1);
-
 // Force the full name to be displayed if we have it
 add_filter('the_author', 'author_full_name');
 function author_full_name($default) {
