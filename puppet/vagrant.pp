965bdb9e606d1baaa87612e7fd57ad7074b77b6b
martent <marten@thavenius.se>
2015-03-22 16:58:52 +0100

    Apache params

--- puppet/vagrant.pp
+++ puppet/vagrant.pp
@@ -14,12 +14,14 @@ class { '::mcommons::mysql':
   db_password      => '',
   db_root_password => '',
   daily_backup     => false,
+  php_enable       => true,
 }
 
 class { '::mcommons::apache':
   force_ssl => true,
   port      => 8000, # set forwarded_port in Vagrantfile
   ssl_port  => 4430, # set forwarded_port in Vagrantfile
-  php       => true,
   snakeoil  => true,
+  php       => true,
+  opcache   => 'Off',
 }
