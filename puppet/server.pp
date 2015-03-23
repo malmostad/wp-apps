6c98da5c2ce139ec946770d26bd72fbe4d5f6a18
martent <marten@thavenius.se>
2015-03-23 21:17:31 +0100

    Vagrant and server Puppet config

--- puppet/server.pp
+++ puppet/server.pp
@@ -3,21 +3,22 @@ $envs = ['production']
 $runner_name  = 'app_runner'
 $runner_group = 'app_runner'
 $runner_home  = '/home/app_runner'
-$runner_path  = "/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
+$runner_path  = '/usr/local/sbin:/usr/local/bin:/usr/bin:/bin'
 
-$app_name       = 'wordpress'
-$app_home       = "${::runner_home}/${::app_name}/wordpress"
+$app_name = 'wordpress'
+$app_home = "${::runner_home}/wordpress-custom/current"
 
 class { '::mcommons': }
 
 class { '::mcommons::mysql':
-  daily_backup => true,
-  php_enable   => true,
+  php_enable => true,
 }
 
 class { '::mcommons::apache':
+  ssl       => true,
   force_ssl => true,
-  snakeoil  => true,
   php       => true,
   opcache   => 'On',
 }
+
+class { '::mcommons::wordpress': }
--- puppet/vagrant.pp
+++ puppet/vagrant.pp
@@ -5,9 +5,8 @@ $runner_group = 'vagrant'
 $runner_home  = '/home/vagrant'
 $runner_path  = '/usr/local/sbin:/usr/local/bin:/usr/bin:/bin'
 
-$app_name       = 'wordpress'
-$app_home       = "${::runner_home}/${::app_name}"
-# $app_home       = '/vagrant'
+$app_name = 'wordpress'
+$app_home = '/vagrant'
 
 class { '::mcommons': }
 
