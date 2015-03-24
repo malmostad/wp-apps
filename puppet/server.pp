71d8b87f4361537a3b74b1251634decc13bbf146
martent <marten@thavenius.se>
2015-03-24 17:00:43 +0100

    Added functionality to just upgrade WP and plugins

--- puppet/server.pp
+++ puppet/server.pp
@@ -9,14 +9,32 @@ $app_name = 'wordpress'
 $app_home = "${::runner_home}/wordpress-custom/current"
 $doc_root = "${::runner_home}/wordpress"
 
-class { '::mcommons': }
+$wp_plugins = [
+  'https://downloads.wordpress.org/plugin/akismet.3.1.1.zip',
+  'https://downloads.wordpress.org/plugin/auto-hyperlink-urls.4.0.zip',
+  'https://downloads.wordpress.org/plugin/content-scheduler.2.0.5.zip',
+  'https://downloads.wordpress.org/plugin/valideratext.2.0.zip',
+  'https://downloads.wordpress.org/plugin/wpdirauth.1.7.6.zip',
+]
 
-class { '::mcommons::mysql':
-  php_enable => true,
+# To just upgrade WP and the plugins as defined in $wp_plugins, run:
+# $ sudo FACTER_WP_UPGRADE=true puppet apply puppet/vagrant.pp
+if $::wp_upgrade {
+  class { '::mcommons::wordpress::install': }
+  ::mcommons::wordpress::install_plugins { $::wp_plugins: }
 }
 
-class { '::mcommons::apache':
-  php => true,
-}
+# Full installation and configuration
+else {
+  class { '::mcommons': }
 
-class { '::mcommons::wordpress': }
+  class { '::mcommons::mysql':
+    php_enable => true,
+  }
+
+  class { '::mcommons::apache':
+    php => true,
+  }
+
+  class { '::mcommons::wordpress': }
+}
--- puppet/vagrant.pp
+++ puppet/vagrant.pp
@@ -9,30 +9,42 @@ $app_name = 'wordpress'
 $app_home = '/vagrant'
 $doc_root = "${::runner_home}/wordpress"
 
-class { '::mcommons': }
+$wp_plugins = [
+  'https://downloads.wordpress.org/plugin/akismet.3.1.1.zip',
+  'https://downloads.wordpress.org/plugin/auto-hyperlink-urls.4.0.zip',
+  'https://downloads.wordpress.org/plugin/content-scheduler.2.0.5.zip',
+  'https://downloads.wordpress.org/plugin/valideratext.2.0.zip',
+  'https://downloads.wordpress.org/plugin/wpdirauth.1.7.6.zip',
+]
 
-class { '::mcommons::mysql':
-  db_password      => '',
-  db_root_password => '',
-  daily_backup     => false,
-  php_enable       => true,
+# To just upgrade WP and the plugins as defined in $wp_plugins, run:
+# $ sudo FACTER_WP_UPGRADE=true puppet apply puppet/vagrant.pp
+if $::wp_upgrade {
+  class { '::mcommons::wordpress::install': }
+  ::mcommons::wordpress::install_plugins { $::wp_plugins: }
 }
 
-class { '::mcommons::apache':
-  port      => 8000,
-  ssl       => false,
-  force_ssl => false,
-  php       => true,
-  opcache   => 'Off',
-}
+# Full installation and configuration
+else {
+  class { '::mcommons': }
 
-class { '::mcommons::wordpress':
-  table_prefix => '',
-  plugins      => [
-    'https://downloads.wordpress.org/plugin/akismet.3.1.1.zip',
-    'https://downloads.wordpress.org/plugin/auto-hyperlink-urls.4.0.zip',
-    'https://downloads.wordpress.org/plugin/content-scheduler.2.0.5.zip',
-    'https://downloads.wordpress.org/plugin/valideratext.2.0.zip',
-    'https://downloads.wordpress.org/plugin/wpdirauth.1.7.6.zip',
-  ]
+  class { '::mcommons::mysql':
+    db_password      => '',
+    db_root_password => '',
+    daily_backup     => false,
+    php_enable       => true,
+  }
+
+  class { '::mcommons::apache':
+    port      => 8000,
+    ssl       => false,
+    force_ssl => false,
+    php       => true,
+    opcache   => 'Off',
+  }
+
+  class { '::mcommons::wordpress':
+    table_prefix => '',
+    plugins      => $::wp_plugins,
+  }
 }
