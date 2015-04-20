351c57ccbf4ff7b9928269e09bd13edb357699de
martent <marten@thavenius.se>
2015-04-20 14:59:06 +0200

    Added plugins for Brightcove shortcodes

---
+++ plugins/bc-video/bc-video.php
@@ -0,0 +1,45 @@
+<?php
+/*
+  Plugin Name: BC Video
+  Plugin URI: https://github.com/malmostad/wp-apps
+  Description: Simple shortcode support to add videos from the
+               Brightcove Cloud in Wordpress posts.
+  Version: 0.0.1
+  Author: Malmö stad
+  Author URI: https://github.com/malmostad
+
+  Instructions:
+    In a Wordpress post, insert the shortcode with the `id`
+    for the video you want to display. You get the id when
+    you manage the video in Brightcove:
+
+    [bc id="123123123"]
+*/
+
+add_shortcode('bc','addBrightcoveVideo');
+
+function addBrightcoveVideo($attrs) {
+	return "
+<div class='bc-video-box'>
+  <script language='JavaScript' type='text/javascript' src='http://admin.brightcove.com/js/BrightcoveExperiences.js'></script>
+  <object id='myExperience4162254067001' class='BrightcoveExperience'>
+    <param name='bgcolor' value='#808080'/>
+    <param name='width' value='480'/>
+    <param name='height' value='270'/>
+    <param name='playerID' value='2810881920001'/>
+    <param name='playerKey' value='AQ~~,AAAArZCmTQE~,w5iz83926flwNgeVE8x1_ZgoF5t7oTGp'/>
+    <param name='isVid' value='true'/>
+    <param name='isUI' value='true'/>
+    <param name='dynamicStreaming' value='true'/>
+    <param name='wmode' value='opaque'/>
+    <param name='htmlFallback' value='true'/>
+    <param name='includeAPI' value='true'/>
+    <param name='templateLoadHandler' value='onTemplateLoad'/>
+    <param name='templateReadyHandler' value='onTemplateReady'/>
+    <param name='@videoPlayer' value='{$attrs["id"]}'/>
+  </object>
+  <script type='text/javascript'>brightcove.createExperiences();</script>
+  <script type='text/javascript' src='http://admin.brightcove.com/js/api/SmartPlayerAPI.js'></script>
+</div>
+";
+}
