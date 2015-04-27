da91105b399b4f3395d035abf13fc08e973cfbd5
martent <marten@thavenius.se>
2015-04-27 16:28:15 +0200

    Sharing script for BC plug

--- plugins/bc-video/bc-video.php
+++ plugins/bc-video/bc-video.php
@@ -20,25 +20,43 @@ add_shortcode('bc','addBrightcoveVideo');
 function addBrightcoveVideo($attrs) {
 	return "
 <div class='bc-video-box'>
-  <script language='JavaScript' type='text/javascript' src='https://sadmin.brightcove.com/js/BrightcoveExperiences.js'></script>
-  <object id='myExperience4162254067001' class='BrightcoveExperience'>
-    <param name='bgcolor' value='#f0f0f0'/>
-    <param name='playerID' value='2810881920001'/>
-    <param name='playerKey' value='AQ~~,AAAArZCmTQE~,w5iz83926flwNgeVE8x1_ZgoF5t7oTGp'/>
-    <param name='isVid' value='true'/>
-    <param name='isUI' value='true'/>
-    <param name='dynamicStreaming' value='true'/>
-    <param name='wmode' value='opaque'/>
-    <param name='htmlFallback' value='true'/>
-    <param name='includeAPI' value='true'/>
-    <param name='templateLoadHandler' value='onTemplateLoad'/>
-    <param name='templateReadyHandler' value='onTemplateReady'/>
-    <param name='secureConnections' value='true'/>
-    <param name='secureHTMLConnections' value='true'/>
-    <param name='@videoPlayer' value='{$attrs["id"]}'/>
-  </object>
-  <script type='text/javascript'>brightcove.createExperiences();</script>
-  <script type='text/javascript' src='https://sadmin.brightcove.com/js/api/SmartPlayerAPI.js'></script>
+	<video
+		id='bc-video'
+	  data-account='745456160001'
+	  data-player='ac887454-ec8b-4ffc-a530-4af5c1c8e8c7'
+		data-video-id='{$attrs["id"]}'
+	  data-embed='default'
+	  class='video-js bc-video' controls></video>
+
+		<script src='//players.brightcove.net/745456160001/ac887454-ec8b-4ffc-a530-4af5c1c8e8c7_default/index.min.js'></script>
+		<link href='//players.brightcove.net/videojs-social/dist/videojs-social.css' rel='stylesheet'>
+		<script src='//players.brightcove.net/videojs-social/dist/videojs-social.min.js'></script>
+		<script>
+		videojs('bc-video').ready(function () {
+		  //Kolla komin:
+		  this.on('loadedmetadata', function (evt) {
+		    if (true || !eval(this.mediainfo.custom_fields.targetgroup === 'Komin')) {
+		      var options = {
+		        'title': this.mediainfo.name,
+		        'description': this.mediainfo.description,
+		        'url': 'http://video.malmo.se?bctid=' + this.mediainfo.id,
+		        'displayAfterVideo': true,
+		        'deeplinking': false,
+		        'offset': '00:00:00',
+		        'services': {
+		          'facebook': true,
+		          'google': true,
+		          'twitter': true,
+		          'tumblr': true,
+		          'pinterest': true,
+		          'linkedin': true
+		        }
+		      };
+		      this.social(options);
+		    }
+		  });
+		});
+		</script>
 </div>
 ";
 }
