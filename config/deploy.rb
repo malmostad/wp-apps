04407b44b396efd48208f19479c99320a5db4c82
martent <marten@thavenius.se>
2015-12-02 17:34:13 +0100

    Plug version def upgrade

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :remote_plugins, [
   'auto-hyperlink-urls.4.0.zip',
   'content-scheduler.2.0.5.zip',
   'valideratext.2.0.zip',
-  'wpdirauth.1.7.6.zip',
+  'wpdirauth.1.7.9.zip',
   'wp-status-notifier.zip'
 ]
 
